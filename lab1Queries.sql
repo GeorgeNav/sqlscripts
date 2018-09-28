b1b7740020d1884a08fa4cac483e23eaa9b8aabc
GeorgeNav <george.g.navarro@gmail.com>
2018-09-28 03:44:04 -0500

    Fixed e and f; working on g

--- lab1Queries.sql
+++ lab1Queries.sql
@@ -2,22 +2,22 @@
     -- -- -- -- -- --  Queries for LAB 1 -- -- -- -- -- -- 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-/* Question a */
+/* DONE: Question a */
 SELECT S.StudentFirstName, S.StudentLastName, S.StudentGender 
     FROM Students S
         WHERE StudentGender = 'M';
 
-/* Question b */
+/* DONE: Question b */
 SELECT SE. TutorKey, SE.CourseKey, SE.SessionDateKey, SE.StudentKey
     FROM Sessions SE
         WHERE SessionDateKey BETWEEN '01/NOV/09' AND '15/NOV/09';
 
-/* Question c */
+/* DONE: Question c */
 SELECT COUNT(*) AS TotalOver25
     FROM Students
         WHERE StudentAge > '25';
 
-/* Question d */
+/* DONE: Question d */
 -- Returns Empty Set. Will need to confirm.
 SELECT SE.TutorKey AS TotalSessions
     FROM Sessions SE
@@ -25,7 +25,7 @@ SELECT SE.TutorKey AS TotalSessions
     GROUP BY SE.TutorKey
         HAVING COUNT(*) < 4;
 
-/* Question e */
+/* DONE: Question e */
 -- Date is a keyword. Cannot use it as a column name
 DROP VIEW vw_Sessions;
 CREATE OR REPLACE VIEW vw_Sessions AS
@@ -36,4 +36,31 @@ CREATE OR REPLACE VIEW vw_Sessions AS
         SE.SessionTimeKey AS Time,
         SE.CourseKey AS Course
         FROM Sessions SE, Tutor S;
-SELECT * FROM vw_Sessions;
+
+/* DONE: Question f */
+SELECT DISTINCT Tutor FROM vw_Sessions;
+
+/* Question g */
+DROP TRIGGER SessionHours;
+
+/* TODO: Found this solution online, but does not run */
+CREATE TRIGGER SessionHours
+ON Tutor
+INSTEAD OF INSERT AS
+    BEGIN
+
+    SET NOCOUNT ON;
+    DECLARE @currentMonth INT
+    DECLARE @totalHours REAL
+
+    SET @currentMonth = EXTRACT(YEAR FROM INSERT(Date))
+    SELECT @totalHours = SUM(Sessions)
+    FROM Tutor
+    WHERE Month = @currentMonth
+
+    IF @totalHours > 120
+        BEGIN
+            RAISEERROR('Cant work more than 60 Hours')
+        END
+END
+/
